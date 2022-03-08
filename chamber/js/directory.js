98c7572da7ef1e127a49b4d597c99a88f59480d7
kbellinger <buc18003@byui.edu>
2022-03-08 11:49:59 -0700

    list added

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,5 +1,6 @@
 const requestURL = 'https://kbellinger.github.io/wdd230/chamber/data/businesses.json';
 const cards = document.querySelector('.cards');
+const list = document.querySelector('.list');
 
 function displayBusinessCards(business) {
     // creating elements for the cards
@@ -12,6 +13,9 @@ function displayBusinessCards(business) {
     
     logo.setAttribute('src', business.logo);
     logo.setAttribute('alt', `Logo for ${business.name}.`);
+    
+    web.setAttribute('href', business.website);
+    web.setAttribute('target', 'blank');
 
     // assigning data to elements within cards
     busName.textContent = `${business.name}`;
@@ -27,12 +31,48 @@ function displayBusinessCards(business) {
 
     cards.appendChild(card);
 
+    // adding to listview
+    // listItem.appendChild(busName);
+    // listItem.appendChild(address);
+    // listItem.appendChild(phone);
+    // listItem.appendChild(web);
+
+    // list.appendChild(listItem);
+
 };
+
+function displayList(business) {
+    let listItem = document.createElement('section');
+    let busName = document.createElement('h2');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let web = document.createElement('a');
+
+    web.setAttribute('href', business.website);
+    web.setAttribute('target', 'blank');
+
+    busName.textContent = `${business.name}`;
+    address.textContent = `${business.address}`;
+    phone.textContent = `${business.phone}`;
+    web.textContent = `${business.website}`;
+
+     // adding to listview
+    listItem.appendChild(busName);
+    listItem.appendChild(address);
+    listItem.appendChild(phone);
+    listItem.appendChild(web);
+
+    list.appendChild(listItem);
+}
 
 // toggle list view
 function listView () {
-    document.getElementById('list').classList.toggle('')
+    document.querySelector('.list').classList.toggle('list-open');
+    document.getElementById('listviewbtn').classList.toggle('list-open');
 }
+const listBtn = document.getElementById('listviewbtn')
+listBtn.onclick = listView;
+
 
 fetch(requestURL)
 .then (function (response) {
