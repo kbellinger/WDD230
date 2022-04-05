847e183e29ba1a1cc8f62566eedef30cd43e2cf8
kbellinger <buc18003@byui.edu>
2022-04-05 17:41:20 -0600

    temple cards

--- temple-inn-project/js/temples.js
+++ temple-inn-project/js/temples.js
@@ -4,39 +4,39 @@ const cards = document.querySelector('#temple-cards');
 function displayTemples(temple){
     let card = document.createElement('section');
     let photo = document.createElement('img');
-    let name = document.createElement('h2');
+    let name = document.createElement('h3');
     let address = document.createElement('p');
     let phone = document.createElement('p');
     let email = document.createElement('p');
-    let service = document.createElement('p');
-    let history = document.createElement('p');
-    let schedule = document.createElement('a');
-    let closure = document.createElement('p');
+    // let service = document.createElement('p');
+    // let history = document.createElement('p');
+    // let schedule = document.createElement('a');
+    // let closure = document.createElement('p');
 
 
     photo.setAttribute('src', temple.image);
     photo.setAttribute('alt', `Photo of ${temple.name}.`)
 
-    schedule.setAttribute('href', temple.schedule);
-    schedule.setAttribute('target', 'blank');
+    // schedule.setAttribute('href', temple.schedule);
+    // schedule.setAttribute('target', 'blank');
 
     name.textContent = `${temple.name}`;
     address.textContent = `${temple.address}`;
     phone.textContent = `${temple.phone}`;
     email.textContent = `${temple.email}`;
-    service.textContent = `${temple.services}`;
-    history.textContent = `${temple.history}`;
-    closure.textContent = `${temple.closure}`;
+    // service.textContent = `${temple.services}`;
+    // history.textContent = `${temple.history}`;
+    // closure.textContent = `${temple.closure}`;
 
     card.appendChild(photo);
     card.appendChild(name);
     card.appendChild(address);
     card.appendChild(phone);
     card.appendChild(email);
-    card.appendChild(service);
-    card.appendChild(history);
-    card.appendChild(closure);
-    card.appendChild(schedule);
+    // card.appendChild(service);
+    // card.appendChild(history);
+    // card.appendChild(closure);
+    // card.appendChild(schedule);
 
     cards.appendChild(card)
 
