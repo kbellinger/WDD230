3edd29a971b472a44eb66c3887797f07aba29f7b
kbellinger <buc18003@byui.edu>
2022-04-05 17:05:40 -0600

    edited temple json

--- temple-inn-project/js/temples.js
+++ temple-inn-project/js/temples.js
@@ -1,1 +1,6 @@
-const
+const requestURL = 'https://kbellinger.github.io/wdd230/temple-inn-project/data/temples.json';
+const card = document.querySelector('#temple-cards');
+
+function displayTemples(temple){
+    let card = document.createElement('section');
+}
