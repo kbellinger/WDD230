f0e30ca8d53dfe912377671b2379be1f92bb938b
kbellinger <buc18003@byui.edu>
2022-04-05 17:26:53 -0600

    photos switched

--- temple-inn-project/js/temples.js
+++ temple-inn-project/js/temples.js
@@ -1,6 +1,54 @@
-const requestURL = 'https://kbellinger.github.io/wdd230/temple-inn-project/data/temples.json';
-const card = document.querySelector('#temple-cards');
+const requestURL = 'https://kbellinger.github.io/wdd230/temple-inn-project/data/temple-list.json';
+const cards = document.querySelector('#temple-cards');
 
 function displayTemples(temple){
     let card = document.createElement('section');
+    let photo = document.createElement('img');
+    let name = document.createElement('h2');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let email = document.createElement('p');
+    let service = document.createElement('p');
+    let history = document.createElement('p');
+    let schedule = document.createElement('a');
+    let closure = document.createElement('p');
+
+
+    photo.setAttribute('src', temple.image);
+    photo.setAttribute('alt', `Photo of ${temple.name}.`)
+
+    schedule.setAttribute('href', temple.schedule);
+    schedule.setAttribute('target', 'blank');
+
+    name.textContent = `${temple.name}`;
+    address.textContent = `${temple.address}`;
+    phone.textContent = `${temple.phone}`;
+    email.textContent = `${temple.email}`;
+    service.textContent = `${temple.services}`;
+    history.textContent = `${temple.history}`;
+    closure.textContent = `${temple.closure}`;
+
+    card.appendChild(photo);
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(email);
+    card.appendChild(service);
+    card.appendChild(history);
+    card.appendChild(closure);
+    card.appendChild(schedule);
+
+    cards.appendChild(card)
+
+
 }
+
+// fetching json file
+fetch(requestURL)
+.then (function (response) {
+    return response.json();
+})
+.then(function(jsonObject){
+    const temples = jsonObject['temples'];
+    temples.forEach(displayTemples);
+});
