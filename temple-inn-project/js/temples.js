c86127d38a26c8b43b331ba58e2083bd1c7b3bf9
kbellinger <buc18003@byui.edu>
2022-04-06 00:32:48 -0600

    tmeple page finished

--- temple-inn-project/js/temples.js
+++ temple-inn-project/js/temples.js
@@ -8,35 +8,61 @@ function displayTemples(temple){
     let address = document.createElement('p');
     let phone = document.createElement('p');
     let email = document.createElement('p');
-    // let service = document.createElement('p');
-    // let history = document.createElement('p');
-    // let schedule = document.createElement('a');
-    // let closure = document.createElement('p');
+    let info = document.createElement('section');
+    let serviceH = document.createElement('h4')
+    let service = document.createElement('p');
+    let historyH = document.createElement('h4')
+    let history = document.createElement('p');
+    let schedule = document.createElement('a');
+    let schedulebtn = document.createElement('button');
+    let closings = document.createElement('h4');
+    let closure = document.createElement('p');
 
 
     photo.setAttribute('src', temple.image);
     photo.setAttribute('alt', `Photo of ${temple.name}.`)
 
-    // schedule.setAttribute('href', temple.schedule);
-    // schedule.setAttribute('target', 'blank');
+    card.setAttribute('id', 'card');
+
+    info.setAttribute('id', 'info');
+
+    schedule.setAttribute('href', temple.schedule);
+    schedule.setAttribute('target', 'blank');
+
+    schedulebtn.setAttribute('class', 'sched-btn')
 
     name.textContent = `${temple.name}`;
     address.textContent = `${temple.address}`;
     phone.textContent = `${temple.phone}`;
     email.textContent = `${temple.email}`;
-    // service.textContent = `${temple.services}`;
-    // history.textContent = `${temple.history}`;
-    // closure.textContent = `${temple.closure}`;
+    schedulebtn.textContent = `Schedule`
+    // dropdownbtn.textContent = `More Info`
+    serviceH.textContent = `Services`;
+    service.textContent = `${temple.services}`;
+    historyH.textContent = `History`;
+    history.textContent = `${temple.history}`;
+    closings.textContent = `Closings`;
+    closure.textContent = `${temple.closure}`;
+
+
+
+    schedule.appendChild(schedulebtn);
 
     card.appendChild(photo);
     card.appendChild(name);
     card.appendChild(address);
     card.appendChild(phone);
     card.appendChild(email);
-    // card.appendChild(service);
-    // card.appendChild(history);
-    // card.appendChild(closure);
-    // card.appendChild(schedule);
+    card.appendChild(schedule);
+
+    info.appendChild(serviceH);
+    info.appendChild(service);
+    info.appendChild(historyH);
+    info.appendChild(history);
+    info.appendChild(closings);
+    info.appendChild(closure);
+
+    card.appendChild(info)
 
     cards.appendChild(card)
 
@@ -52,3 +78,43 @@ fetch(requestURL)
     const temples = jsonObject['temples'];
     temples.forEach(displayTemples);
 });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function infoFlip() {
+
+}
+
+function draperDrop() {
+    document.querySelector('.info-draper').classList.toggle("show");
+    document.querySelector('dropbtn').classList.toggle('show');
+  }
+
+function jrDrop() {
+    document.querySelector('.info-jr').classList.toggle("show");
+    document.querySelector('dropbtn').classList.toggle('show');
+}
+
+function omDrop() {
+    document.querySelector('.info-om').classList.toggle("show");
+    document.querySelector('dropbtn').classList.toggle('show');
+}
+
+function slcDrop() {
+    document.querySelector('.info-slc').classList.toggle("show");
+    document.querySelector('dropbtn').classList.toggle('show');
+}
+  
+//   Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName('more-info');
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
